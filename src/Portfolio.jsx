a8c6b7db6bf2b431f4913991f3e3cfaeb8468b70
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2022-02-28 00:47:57 -0800

    project structure and navbar completed

---
+++ src/Portfolio.jsx
@@ -0,0 +1,12 @@
+import "./scss/Portfolio.scss";
+import Navbar from "./components/Navbar";
+
+function Portfolio() {
+  return (
+    <div className="Portfolio">
+      <Navbar />
+    </div>
+  );
+}
+
+export default Portfolio;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,67 @@
+import "../scss/_navbar.scss";
+import logo from "../assets/logo.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faClose } from "@fortawesome/free-solid-svg-icons";
+import { useRef } from "react";
+
+export default function Navbar() {
+  const dropDownRef = useRef(null);
+
+  const toggleDropDown = () => {
+    const displayValue = dropDownRef.current.style.display;
+    if (displayValue === "" || displayValue === "none") {
+      dropDownRef.current.style.display = "flex";
+    } else {
+      dropDownRef.current.style.display = "none";
+    }
+  };
+
+  return (
+    <nav className="Navbar">
+      <img src={logo} alt="" />
+      <FontAwesomeIcon
+        onClick={toggleDropDown}
+        icon={faBars}
+        className="hamburger"
+      />
+
+      <ul className="expanded-menu">
+        <li>
+          <span className="menu-numbering">01.</span>About
+        </li>
+        <li>
+          <span className="menu-numbering">02.</span>Projects
+        </li>
+        <li>
+          <span className="menu-numbering">03.</span>Contact
+        </li>
+        <li>
+          <span className="menu-resume">Resume</span>
+        </li>
+      </ul>
+
+      <div ref={dropDownRef} className="dropDown">
+        <FontAwesomeIcon
+          icon={faClose}
+          className="close-dropDown"
+          onClick={toggleDropDown}
+        />
+
+        <ul className="expanded-menu">
+          <li>
+            <span className="menu-numbering">01.</span>About
+          </li>
+          <li>
+            <span className="menu-numbering">02.</span>Projects
+          </li>
+          <li>
+            <span className="menu-numbering">03.</span>Contact
+          </li>
+          <li>
+            <span className="menu-resume">Resume</span>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
