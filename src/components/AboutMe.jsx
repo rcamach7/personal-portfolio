df713c281119b7b26b9b8ced9b680450da354b22
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2022-02-28 18:33:32 -0800

    all content for about me has been added

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,7 +1,41 @@
+import skillData from "../assets/skills.json";
+
 export default function AboutMe() {
+  const skillCards = skillData.map((skillItem, i) => (
+    <SkillCard key={i} skill={skillItem} />
+  ));
   return (
     <section className="AboutMe">
-      <h1>Hello World</h1>
+      <h2 className="section-title">
+        <span className="numbering">01.</span>About Me
+      </h2>
+
+      <div className="aboutme-picture">
+        <div className="picture-container" />
+      </div>
+
+      <div className="aboutme-information">
+        <p className="info">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec
+          odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.
+        </p>
+        <p className="info">
+          Curabitur sodales ligula in libero. Sed dignissim lacinia nunc.
+          Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem
+          at dolor. Maecenas mattis.
+        </p>
+
+        <div className="skills-container">{skillCards}</div>
+      </div>
     </section>
+  );
+}
+
+function SkillCard(props) {
+  return (
+    <div className="SkillCard">
+      <img src={props.skill.img} alt="" />
+      <p>{props.skill.skill}</p>
+    </div>
   );
 }
