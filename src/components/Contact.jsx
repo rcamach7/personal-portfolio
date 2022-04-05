d1f0e3de4fec5e518bb633c5352f4a3adc63ae05
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2022-04-04 20:17:38 -0700

    implement statif form to send message with theme styling

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,6 +5,7 @@ import {
   faFacebook,
   faDev,
 } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 export default function Contact() {
   return (
@@ -13,18 +14,32 @@ export default function Contact() {
         <span className="numbering">03.</span> What's Next?
       </h2>
 
-      <div className="contact-me reveal">
-        <h1 className="contact-me">Get In Touch!</h1>
-        <p className="status">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris.Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris.
-        </p>
-        <address>
-          <a href="mailto:ricmireles7@gmail.com">Say Hello</a>
+      <form className="contact-form reveal">
+        <div className="formTitle">
+          <FontAwesomeIcon icon={faEnvelope} className="icon" />
+          <p>Get in touch!</p>
+        </div>
+        <input className="inputField" type="text" placeholder="Your name" />
+        <input
+          className="inputField"
+          type="email"
+          placeholder="Your email"
+          required
+        />
+        <textarea
+          className="inputField"
+          cols="30"
+          rows="5"
+          placeholder="Your message"
+          required
+        />
+
+        <input type="submit" className="submit-btn" value="Send message" />
+        {/* Use default mailer instead */}
+        <address className="default-mailer">
+          <a href="mailto:ricmireles7@gmail.com">use default mailer instead</a>
         </address>
-      </div>
+      </form>
 
       <div className="contact-links">
         <a
